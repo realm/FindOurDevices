bcbc245dffd781a97dc39fd6f8d01bdd9a2a4326
LJ <81748770+elle-j@users.noreply.github.com>
2021-08-03 11:51:39 +0200

    Add useGroup hook, remove GroupsProvider

---
+++ app/hooks/useGroup.js
@@ -0,0 +1,91 @@
+import { useState, useEffect, useRef } from 'react';
+import Realm, { BSON } from 'realm';
+
+import { useAuth } from '../providers/AuthProvider';
+import Group from '../models/Group';
+import GroupMember from '../models/GroupMember';
+import Location from '../models/Location';
+
+// For complimentary comments on the use of Realm in this module, see
+// /app/providers/AuthProvider.js as it follows a similar structure
+
+function useGroup(groupId) {
+  const { realmUser } = useAuth();
+  const [group, setGroup] = useState(null);
+  const realmRef = useRef();
+
+  useEffect(() => {
+    if (!realmUser)
+      return;
+
+    openRealm();
+
+    return closeRealm;
+  }, [realmUser]);
+
+  const openRealm = async () => {
+    try {
+      // Open a local realm file with the schemas that are part of this partition
+      const config = {
+        schema: [Group.schema, GroupMember.schema, Location.schema],
+        sync: {
+          user: realmUser,
+          partitionValue: `group=${groupId}`,
+          // Add a callback on the 'error' property to log any sync errors while developing
+          error: (session, syncError) => {
+            console.error('syncError.name: ', syncError.name);
+            if (syncError.message)
+              console.error('syncError.message: ', message);
+          }
+        }
+      };
+
+      const realm = Realm.exists(config)
+        ? new Realm(config)
+        : await Realm.open(config);
+
+      realmRef.current = realm;
+
+      const group = realm.objectForPrimaryKey('Group', new BSON.ObjectId(groupId));
+      if (group)
+        setGroup(group);
+
+      group.addListener((/*collection, changes*/) => {
+        setGroup(realm.objectForPrimaryKey('Group', new BSON.ObjectId(groupId)));
+      });
+    }
+    catch (err) {
+      console.error('Error opening realm: ', err.message);
+    }
+  };
+
+  const closeRealm = () => {
+    const realm = realmRef.current;
+    realm?.removeAllListeners();
+    realm?.close();
+    realmRef.current = null;
+    setGroup([]);
+  };
+
+  const setGroupName = (name) => {
+    // We can call our configured MongoDB Realm functions as methods on the User.functions
+    // property (as seen below), or by passing the function name and its arguments to
+    // User.callFunction('functionName', args). When the backend changes the group name on
+    // the Group, it will also change it on the embedded GroupMembership of each of its Users.
+    // Since the changes apply to synced realms, the notification handler that we passed to
+    // the change listener will get called and we can subsequently update the UI state.
+    // (Currently we only show changes once synced from the server, thus not offline)
+
+    // TODO: Implement the backend function setGroupName
+    return realmUser.functions.setGroupName(name);
+
+    // NOTE: See if caller should try-catch the call instead
+  };
+
+  return { group/*, setGroupName*/ };
+}
+
+export default useGroup;
+
+// For complimentary comments on the use of Realm in this module, see
+// /app/providers/AuthProvider.js as it follows a similar structure.

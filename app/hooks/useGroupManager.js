e373cc0a04d47736ea29bb981c63f660fbfbd27b
LJ <81748770+elle-j@users.noreply.github.com>
2021-08-05 19:27:02 +0200

    Remove comments explained by backend

--- app/hooks/useGroupManager.js
+++ app/hooks/useGroupManager.js
@@ -6,6 +6,7 @@ function useGroupManager() {
   // We can call our configured MongoDB Realm functions as methods on the User.functions
   // property (as seen below), or by passing the function name and its arguments to
   // User.callFunction('functionName', args).
+  
   // Changes made by the backend will be automatically synced by Realm and reacted to
   // via our change listeners (see /app/providers).
   // (Currently we only show changes once synced from the server, thus not offline)
@@ -18,22 +19,14 @@ function useGroupManager() {
   
   const removeGroup = (groupId) => realmUser.functions.removeGroup(groupId.toString());
 
-  const addGroupMember = (groupId, newGroupMemberEmail) => {
-    // When the backend inserts new GroupMember into the 'members' array on the Group,
-    // it will also insert a GroupMembership into the User's 'groups' array.
-    return realmUser.functions.addGroupMember(groupId.toString(), newGroupMemberEmail);
-  };
+  const addGroupMember = (groupId, newGroupMemberEmail) => realmUser.functions.addGroupMember(groupId.toString(), newGroupMemberEmail);
 
   const removeGroupMember = (groupId, memberId) => realmUser.functions.removeGroupMember(groupId.toString(), memberId.toString());
 
   const setShareLocation = (groupId, shareLocation) => realmUser.functions.setShareLocation(groupId.toString(), shareLocation);
 
   // TODO: Implement the backend function setGroupName
-  const setGroupName = (name) => {
-    // When the backend changes the group name on the Group, it will also change it on the
-    // embedded GroupMembership of each of its Users.
-    return realmUser.functions.setGroupName(name);
-  };
+  const setGroupName = (name) => realmUser.functions.setGroupName(name);
 
   return {
     createGroup,

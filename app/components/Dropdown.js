c6a1405749ce94f788849a576eba77965cee43fe
José Martins <zepedromm1998@gmail.com>
2021-08-10 15:11:06 +0100

    Added dropdown to select which device to join group with

---
+++ app/components/Dropdown.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import { View, Text, FlatList, StyleSheet, Pressable } from 'react-native';
+
+import fonts from '../styles/fonts';
+
+export function Dropdown({
+  open,
+  value,
+  items,
+  setOpen,
+  setValue,
+  top
+}) {
+
+  const selectValue = (item) => {
+    setValue(item);
+    setOpen(false);
+  }
+
+  const renderItem = ({ item }) => (
+    <Pressable style={styles.item} onPress={() => { selectValue(item) }}>
+      <View style={{ flexDirection: "row", justifyContent: 'space-between', }}>
+          <Text style={styles.text} > {item.name} </Text>
+          { item.name == value && <Text> ✓ </Text> }
+        </View>
+    </Pressable>
+  );
+
+  return (
+    <View style={styles.container}>
+      {top && open &&
+        <FlatList
+          data={items}
+          renderItem={renderItem}
+          keyExtractor={item => item.id}
+          style={styles.flatlist}
+        />
+      }
+      <Pressable style={[styles.button, !open && { borderRadius: 10 } ]} onPress={() => setOpen(!open)}>
+        <View style={{ flexDirection: "row", justifyContent: 'space-between' }}>
+          <Text style={styles.text}> {value?.name || 'Select a device'} </Text>
+          <Text style={styles.text}> {open ? '∧' : 'v' } </Text>
+        </View>
+      </Pressable>
+      {!top && open &&
+        <FlatList
+          data={items}
+          renderItem={renderItem}
+          keyExtractor={item => item.id}
+          style={styles.flatlist}
+        />
+      }
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignSelf: 'stretch',
+    maxHeight: 200,
+    marginVertical: 10,
+  },
+  button: {
+    borderColor: '#e1e1e1',
+    borderWidth: 1,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    backgroundColor: '#f6f6f6',
+    padding: 10
+  },
+  item: {
+    padding: 10
+  },
+  text : {
+    color: '#6e6969',
+    fontSize: fonts.sizeM
+  },
+  flatlist : {
+    borderColor: '#e1e1e1',
+    backgroundColor: '#f6f6f6',
+    borderWidth: 1,
+    borderTopWidth: 0,
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+  }
+});
--- app/screens/InvitationsScreen.js
+++ app/screens/InvitationsScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Alert, StyleSheet } from 'react-native';
 
 import { useAuth } from '../providers/AuthProvider';
@@ -7,21 +7,32 @@ import { useGroupManager } from '../hooks/useGroupManager';
 import { FormTextInput } from '../components/FormTextInput';
 import { List } from '../components/List';
 import { ModalForm } from '../components/ModalForm';
+import { Dropdown } from '../components/Dropdown';
 
 export function InvitationsScreen() {
   const { userData } = useAuth();
-  //const { devices } = useDevices(); // USE LATER
+  const { devices } = useDevices(); // USE LATER
   const { respondToInvitation } = useGroupManager();
   const [selectedInvitation, setSelectedInvitation] = useState(null);
   const [temporaryDeviceId, setTemporaryDeviceId] = useState('');
 
+  const [pickerIsOpen, setPickerIsOpen] = useState(false);
+  const [pickerValue, setPickerValue] = useState(null);
+  const [pickerItems, setPickerItems] = useState([]);
+  
+  useEffect(() => {
+    setPickerItems(devices.map((device, idx) => ({ name: device.name, id: device._id.toString() })));
+  }, [devices]);
+
   const handleAccept = async () => {
-    if (!selectedInvitation || !temporaryDeviceId)
-      return;
+    /* if (!selectedInvitation || !temporaryDeviceId)
+      return; */
+    if (!pickerValue)
+      return
 
     // Our MongoDB Realm backend function will return an object
     // containing an error property if any errors occurred.
-    const { error } = await respondToInvitation(selectedInvitation.groupId, true, temporaryDeviceId);
+    const { error } = await respondToInvitation(selectedInvitation.groupId, true, pickerValue.id);
     if (error)
       return Alert.alert(error.message);
 
@@ -35,44 +46,52 @@ export function InvitationsScreen() {
     if (temporaryDeviceId)
       setTemporaryDeviceId('');
   };
-  
+
   return (
     <View style={styles.screen}>
       {userData && (
         <>
-        <List
-          items={userData.invitations}
-          keyExtractor={(invitation) => invitation.groupId.toString()}
-          itemTextExtractor={(invitation) => invitation.groupName}
-          itemSubTextExtractor={(invitation) => `From: ${invitation.senderEmail}`}
-          fadeOnPress={false}
-          rightActions={[
-            {
-              actionType: 'accept',
-              onPress: (invitation) => setSelectedInvitation(invitation)
-            },
-            {
-              actionType: 'decline',
-              onPress: (invitation) => respondToInvitation(invitation.groupId, false)
-            }
-          ]}
-        />
-        <ModalForm
-          visible={!!selectedInvitation}
-          title='Accept Invitation'
-          submitText='Accept'
-          onSubmit={handleAccept}
-          onCancel={handleCancelAccept}
-        >
-          <FormTextInput
-            placeholder='Device ID to join with'
-            value={temporaryDeviceId}
-            onChangeText={setTemporaryDeviceId}
-            autoCorrect={false}
-            autoCapitalize='none'
+          <List
+            items={userData.invitations}
+            keyExtractor={(invitation) => invitation.groupId.toString()}
+            itemTextExtractor={(invitation) => invitation.groupName}
+            itemSubTextExtractor={(invitation) => `From: ${invitation.senderEmail}`}
+            fadeOnPress={false}
+            rightActions={[
+              {
+                actionType: 'accept',
+                onPress: (invitation) => setSelectedInvitation(invitation)
+              },
+              {
+                actionType: 'decline',
+                onPress: (invitation) => respondToInvitation(invitation.groupId, false)
+              }
+            ]}
           />
-          {/* TEMPORARY (TODO: add dropdown for device names here) */}
-        </ModalForm>
+          <ModalForm
+            visible={!!selectedInvitation}
+            title='Accept Invitation'
+            submitText='Accept'
+            onSubmit={handleAccept}
+            onCancel={handleCancelAccept}
+          >
+            <Dropdown
+              open={pickerIsOpen}
+              value={pickerValue}
+              items={pickerItems}
+              setOpen={setPickerIsOpen}
+              setValue={setPickerValue}
+              top={false}
+            />
+           {/*  <FormTextInput
+              placeholder='Device ID to join with'
+              value={temporaryDeviceId}
+              onChangeText={setTemporaryDeviceId}
+              autoCorrect={false}
+              autoCapitalize='none'
+            /> */}
+            {/* TEMPORARY (TODO: add dropdown for device names here) */}
+          </ModalForm>
         </>
       )}
     </View>

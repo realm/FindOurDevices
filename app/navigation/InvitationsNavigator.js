4bbe847275df68e28d111f09373135882b792fd9
José Martins <zepedromm1998@gmail.com>
2021-08-11 19:23:26 +0100

    Missing Platform import

--- app/navigation/InvitationsNavigator.js
+++ app/navigation/InvitationsNavigator.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Platform } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import { InvitationsScreen } from '../screens/InvitationsScreen';

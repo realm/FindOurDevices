501f67642556eccb8afeae3ed503285ff3028d8e
José Martins <zepedromm1998@gmail.com>
2021-08-12 11:32:14 +0100

    Showing view map button only if exist devices

--- app/screens/DevicesScreen.js
+++ app/screens/DevicesScreen.js
@@ -10,7 +10,7 @@ import routes from '../navigation/routes';
 
 export function DevicesScreen({ navigation }) {
   const { devices, currentIosOrAndroidId, addCurrentDevice } = useDevices();
-  const { isOn: modalVisible, turnOff: closeModal }= useToggle(false, navigation, 'plus-circle');
+  const { isOn: modalVisible, turnOff: closeModal } = useToggle(false, navigation, 'plus-circle');
 
   const handleAddDevice = async () => {
     const res = await addCurrentDevice();
@@ -19,39 +19,43 @@ export function DevicesScreen({ navigation }) {
 
     closeModal();
   };
-  
+
   return (
     <>
-    <View style={styles.screen}>
-      <List
-        items={devices}
-        keyExtractor={(device) => device._id.toString()}
-        itemTextExtractor={(device) => device.name}
-        itemSubTextExtractor={(device) => device.iosOrAndroidId === currentIosOrAndroidId ? 'current' : '' }
-        fadeOnPress={false}
-        rightActions={[
-          {
-            actionType: 'remove',
-            onPress: (device) => console.log(`Pressed btn to remove ${device.name}.`)
-          }
-        ]}
-        emptyListText='Add your first device.'
-      />
-      <View style={styles.buttonContainer}>
-        <Button
-          text='View Map'
-          onPress={() => navigation.navigate(routes.DEVICES_MAP)}
-          style={{ marginBottom: 30 }}
+      <View style={styles.screen}>
+        <List
+          items={devices}
+          keyExtractor={(device) => device._id.toString()}
+          itemTextExtractor={(device) => device.name}
+          itemSubTextExtractor={(device) => device.iosOrAndroidId === currentIosOrAndroidId ? 'current' : ''}
+          fadeOnPress={false}
+          rightActions={[
+            {
+              actionType: 'remove',
+              onPress: (device) => console.log(`Pressed btn to remove ${device.name}.`)
+            }
+          ]}
+          emptyListText='Add your first device.'
         />
+        {
+          devices?.length > 0 &&
+          <View style={styles.buttonContainer}>
+            <Button
+              text='View Map'
+              onPress={() => navigation.navigate(routes.DEVICES_MAP)}
+              style={{ marginBottom: 30 }}
+            />
+          </View>
+        }
+
       </View>
-    </View>
-    <ModalForm
-      visible={modalVisible}
-      title='Add Current Device?'
-      submitText='Yes'
-      onSubmit={handleAddDevice}
-      onCancel={closeModal}
-    />
+      <ModalForm
+        visible={modalVisible}
+        title='Add Current Device?'
+        submitText='Yes'
+        onSubmit={handleAddDevice}
+        onCancel={closeModal}
+      />
     </>
   );
 }
